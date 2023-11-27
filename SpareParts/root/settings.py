26675dd2f58f37dcd7a3c337a97613de083e28d6
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-11-27 16:13:07 +0400

    Определение директории для хранения шаблонов

--- SpareParts/root/settings.py
+++ SpareParts/root/settings.py
@@ -55,10 +55,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'root.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'template')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
