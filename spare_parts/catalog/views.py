14a5c336bd0104b669f9c287b07f0584071429ed
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-12-17 11:26:26 +0400

    первая страница

--- spare_parts/catalog/views.py
+++ spare_parts/catalog/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ProductCard, Photo, Brand
 
 
 def index(request):
-    return HttpResponse("<h1>Main page</h1>")
+    text_head = 'Каталог доступных запчастей'
+    spares = ProductCard.objects.all()
+    num_spares = ProductCard.objects.all().count()
+    brand = Brand.objects
+    num_brand = Brand.objects.count()
+    photo = Photo.objects.all()
+
+    context = {'text_head': text_head, 
+               'spares': spares, 
+               'num_spares': num_spares,
+               'brand': brand,
+               'num_brand': num_brand,
+               'photo': photo}
+    
+    return render(request, 'catalog/index.html', context) 
