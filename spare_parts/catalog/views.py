025a0df74b4a84892922e02cec459b619fb2a728
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-12-22 18:06:44 +0400

    Добавление функций для отображеия страниц О нас и Контакты

--- spare_parts/catalog/views.py
+++ spare_parts/catalog/views.py
@@ -36,3 +36,41 @@ def index(request):
                'photo': photo}
     
     return render(request, 'catalog/index.html', context) 
+
+
+def about(request):
+    text_head = 'Сведения о компании'
+    name = 'Авторазбор г. Тольятти'
+    used_spare_parts = ' Продажа б/у автозапчастей'
+    used_tires = 'Продажа б/у шин'
+    used_rims = 'Продажа б/у дисков'
+    car_buyback = 'Выкуп автомобиля в любом состоянии'
+    
+    context = {
+        'text_head': text_head,
+        'name': name,
+        'used_spare_parts': used_spare_parts,
+        'used_tires': used_tires,
+        'used_rims': used_rims,
+        'car_buyback': car_buyback
+    }
+    
+    return render(request, 'catalog/about.html', context)
+   
+   
+def contact(request):
+    text_head = 'Контакты'
+    name = 'Авторазбор г. Тольятти'
+    address = 'г. Тольятти, ул. Примерная, 123'
+    phone = '+7 123 456 789'
+    mail = 'avtorazbor@yandex.ru'
+    
+    context = {
+        'text_head': text_head,
+        'name': name,
+        'address': address,
+        'phone': phone,
+        'mail': mail
+    }
+    
+    return render(request, 'catalog/contact.html', context)
