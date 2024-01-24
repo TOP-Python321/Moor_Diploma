2a7a4a49d7c9e33557e55dc4cdce971e7367e839
Schwarzepolizei <schwarzepolizei@gmail.com>
2024-01-24 16:50:49 +0400

    Маршрут для регистрации пользователей

--- spare_parts/catalog/urls.py
+++ spare_parts/catalog/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from .import views
 
 
 urlpattern = [
     path('', views.index, name='index'), 
     path('spares/', views.SparesListView.as_view(), name='spares'),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('spare_parts.urls')),
 ]
